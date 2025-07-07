68c1a74a7a9307637d07855cdb3f4e76f4dc5597
Rohit Zirmute <rohitzirmute821@gmail.com>
2025-07-07 16:02:29 +0530

    changed cors

--- src/main/java/com/example/e_learning/config/SecurityConfig.java
+++ src/main/java/com/example/e_learning/config/SecurityConfig.java
@@ -20,9 +20,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 
 @Configuration
 @EnableWebSecurity
