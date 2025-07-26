5f52f70db31c5a724799335f7e7849fe76cbeba4
Rohit Zirmute <rohitzirmute821@gmail.com>
2025-07-27 01:35:08 +0530

    Upload fresh JAR with @Lazy in SecurityConfig to fix circular dependency

--- src/main/java/com/example/e_learning/config/SecurityConfig.java
+++ src/main/java/com/example/e_learning/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.e_learning.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -22,9 +23,9 @@ public class SecurityConfig {
 
     private JwtAuthFilter jwtAuthFilter;
 
-    // Setter injection to break circular dependency
+    // Setter injection with @Lazy to break circular dependency
     @Autowired
-    public void setJwtAuthFilter(JwtAuthFilter jwtAuthFilter) {
+    public void setJwtAuthFilter(@Lazy JwtAuthFilter jwtAuthFilter) {
         this.jwtAuthFilter = jwtAuthFilter;
     }
 
